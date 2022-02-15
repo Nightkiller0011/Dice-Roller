26e5b099766ba42ce96d441c41a39a55c02d1908
macbeth-byui <macbethc@byui.edu>
2022-02-15 12:38:26 -0700

    Updated

--- app/src/main/java/macbeth/tempconverter/MainActivity.kt
+++ app/src/main/java/macbeth/tempconverter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import macbeth.tempconverter.databinding.ActivityMainBinding
 
 /*
 The MainActivity represents a single screen on the App.  This entire program will be a single
@@ -13,9 +15,12 @@ screen app.  AppCompatActivity is an Android SDK class that represents an Activi
 When Android wants to display this screen, it will run the code in the onCreate function.
  */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var temp_input : EditText
+    private lateinit var temp_output : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Associate the layout XML with this activity so Android knows what to draw
         setContentView(R.layout.activity_main)
 
@@ -25,39 +30,39 @@ class MainActivity : AppCompatActivity() {
         val c_to_f_button = findViewById<Button>(R.id.c_to_f_button)
         val reset_button = findViewById<Button>(R.id.reset_button)
 
+        temp_input = findViewById<EditText>(R.id.temp_input)
+        temp_output = findViewById<TextView>(R.id.temp_output)
+
         // Define which functions to run if a button is pressed
         f_to_c_button.setOnClickListener { convert_f_to_c() }
         c_to_f_button.setOnClickListener { convert_c_to_f() }
         reset_button.setOnClickListener { reset() }
+
     }
 
     private fun convert_f_to_c() {
-        // Obtain references to the input/output fields in the layout XML
-        val temp_input = findViewById<EditText>(R.id.temp_input)
-        val temp_output = findViewById<TextView>(R.id.temp_output)
-
         // Convert the input to a float, calculate the result, and convert it to a string
-        val f_temp = temp_input.text.toString().toFloat()
-        val c_temp = (f_temp - 32.0f) * (5.0f/9.0f)
-        temp_output.text = "${"%.1f".format(c_temp)} C"
+        try {
+            val f_temp = temp_input.text.toString().toFloat()
+            val c_temp = (f_temp - 32.0f) * (5.0f / 9.0f)
+            temp_output.text = "${"%.1f".format(c_temp)} C"
+        } catch (e : Exception) {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun convert_c_to_f() {
-        // Obtain references to the input/output fields in the layout XML
-        val temp_input = findViewById<EditText>(R.id.temp_input)
-        val temp_output = findViewById<TextView>(R.id.temp_output)
-
         // Convert the input to a float, calculate the result, and convert it to a string
-        val c_temp = temp_input.text.toString().toFloat()
-        val f_temp = (c_temp * 9.0f / 5.0f) + 32.0f
-        temp_output.text = "${"%.1f".format(f_temp)} F"
+        try {
+            val c_temp = temp_input.text.toString().toFloat()
+            val f_temp = (c_temp * 9.0f / 5.0f) + 32.0f
+            temp_output.text = "${"%.1f".format(f_temp)} F"
+        } catch (e : Exception) {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun reset() {
-        // Obtain references to the input/output fields in the layout XML
-        val temp_input = findViewById<EditText>(R.id.temp_input)
-        val temp_output = findViewById<TextView>(R.id.temp_output)
-
         // Clear out the input/output fields
         temp_input.text.clear()
         temp_output.text = ""
